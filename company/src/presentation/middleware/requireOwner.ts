5c24bb8658ca164ebcace44c8b57d69276a0c968
nabeel-ncz <nabeelnabhan115@gmail.com>
2024-05-30 10:04:01 +0530

    fix: company mangement previlage

--- company/src/presentation/middleware/requireOwner.ts
+++ company/src/presentation/middleware/requireOwner.ts
@@ -1,5 +1,14 @@
 import { findUserById } from "@/infrastructure/database/postgres/repositories";
+import { ForbiddenError, UnAuthorizedError } from "@company-management/common";
 import { NextFunction, Request, Response } from "express";
+
+export enum UserRole {
+    ADMIN = "admin",
+    OWNER = "owner",
+    CLIENT = "client",
+    OPS = "ops",
+    INTERNAL = "internal"
+}
 
 export const requireOwner = async (
     req: Request,
@@ -7,8 +16,17 @@ export const requireOwner = async (
     next: NextFunction
 ) => {
     try {
-        
+        if(!req.user.id) {
+            throw new UnAuthorizedError();
+        }
+        const user = await findUserById(Number(req.user.id));
+        if(user.role !== UserRole.OWNER) {
+            if(user.role !== UserRole.ADMIN) {
+                throw new ForbiddenError();
+            }
+        }
+        next();
     } catch (error) {
-
+        next(error);
     }
 }
