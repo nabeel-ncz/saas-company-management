9a996aa5b9469ee8bd13184aa854a635271a94b6
nabeel-ncz <nabeelnabhan115@gmail.com>
2024-05-30 10:11:51 +0530

    fix: kafka message producing

--- auth/src/infrastructure/messages/kafka/producer/emailVerificationProducer.ts
+++ auth/src/infrastructure/messages/kafka/producer/emailVerificationProducer.ts
@@ -1,15 +1,16 @@
 import { producer } from "@/infrastructure/messages/kafka";
+import { NOTIFICATION_SERVICE_TOPIC, USER_EMAIL_VERFICATION } from "@company-management/common"
 
 export default async function (
     data: { email: string, otp: string }
-){
+) {
 
     try {
         await producer.connect();
         await producer.send({
-            topic: `NOTIFICATION_SERVICE_TOPIC`,
+            topic: NOTIFICATION_SERVICE_TOPIC,
             messages: [{
-                key: `emailVerificationRequested`,
+                key: USER_EMAIL_VERFICATION,
                 value: JSON.stringify(data)
             }]
         });
