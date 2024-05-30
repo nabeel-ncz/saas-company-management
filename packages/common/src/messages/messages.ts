e75bdb3ae6a59d785198897259b487c64c438f19
nabeel-ncz <nabeelnabhan115@gmail.com>
2024-05-30 15:51:38 +0530

    feat: added new message kafka subscriber interface

--- packages/common/src/messages/messages.ts
+++ packages/common/src/messages/messages.ts
@@ -2,5 +2,6 @@ export const USER_CREATED_MESSAGE = "userCreated";
 export const USER_UPDATED_MESSAGE = "userUpdated";
 export const EMPLOYEE_CREATED_MESSAGE = "employeeCreated";
 export const EMPLOYEE_UPDATED_MESSAGE = "employeeUpdated";
+export const EMPLOYEE_DELETED_MESSAGE = "employeeDeleted";
 export const USER_EMAIL_VERFICATION = "userEmailVerfication";
 export const NOTIFY_EMPLOYEE_CREATION = "notifyEmployeeCreation";
