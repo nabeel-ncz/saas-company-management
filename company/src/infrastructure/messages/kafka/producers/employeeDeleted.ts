b3fb506edee05085abee7ce879731edc6caa3e73
nabeel-ncz <nabeelnabhan115@gmail.com>
2024-05-30 16:38:21 +0530

    feat: employee deleted message produced to kafka

--- company/src/infrastructure/messages/kafka/producers/employeeDeleted.ts
+++ company/src/infrastructure/messages/kafka/producers/employeeDeleted.ts
@@ -1,3 +1,39 @@
-export default async function (data) {
+import {
+    AUTH_SERVICE_TOPIC,
+    EMPLOYEE_DELETED_MESSAGE,
+    USER_SERVICE_TOPIC
+} from "@company-management/common";
+import { producer } from "..";
 
+export default async function (data) {
+    try {
+        await producer.connect();
+        await producer.sendBatch({
+            topicMessages: [
+                {
+                    topic: USER_SERVICE_TOPIC,
+                    messages: [{
+                        key: EMPLOYEE_DELETED_MESSAGE,
+                        value: JSON.stringify(data)
+                    }]
+                },
+                {
+                    topic: AUTH_SERVICE_TOPIC,
+                    messages: [{
+                        key: EMPLOYEE_DELETED_MESSAGE,
+                        value: JSON.stringify(data)
+                    }]
+                }
+            ]
+        });
+        console.log(`
+        ===========================================
+        EMPLOYEE_DELETED_MESSAGE => [USER_SERVICE_TOPIC, AUTH_SERVICE_TOPIC]
+        ===========================================
+        `)
+    } catch (error: any) {
+        console.error('kafka produce error : ', error?.message);
+    } finally {
+        await producer.disconnect();
+    }
 }
